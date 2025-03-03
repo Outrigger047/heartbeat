2eadcca62cd3c13d21ca7a990e3fae2872407ed8
Andrew Wyshak <andrew.wyshak@gmail.com>
2025-03-03 07:40:06 -0500

    Transfer machines

--- Program.cs
+++ Program.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using System.Text.RegularExpressions;
 
 namespace Heartbeat
 {
     class Program
     {
+        private static string configOptionMatch = @"\:.+$";
+        private static string? configPath = null;
+        private static int? configFreqSec = null;
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            // Read configuration file
+            foreach (var line in File.ReadLines(".configuration"))
+            {
+                if (Regex.IsMatch(line, "^configPath"))
+                {
+                    configPath = GetConfigOptionString(line);
+                }
+                else if (Regex.IsMatch(line, "^configFreqSec"))
+                {
+                    configFreqSec = int.Parse(GetConfigOptionString(line));
+                }
+            }
+
+            if (configPath != null && configFreqSec != null)
+            {
+                var updater = new PageUpdater(configPath);
+                updater.ReadPage();
+
+                do
+                {
+                    
+                } while(true);
+            }
+        }
+
+        private static string GetConfigOptionString(string lineIn)
+        {
+            return Regex.Match(lineIn, configOptionMatch).ToString().TrimStart(':');
         }
     }
 }
